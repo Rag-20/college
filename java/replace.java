fe20ea1e8bac1c856526d7df2ceb2caf61c3d11f
Rag-20 <160122299+Rag-20@users.noreply.github.com>
2025-01-12 18:06:15 +0530

    Delete java directory

--- java/replace.java
+++
@@ -1,8 +0,0 @@
-public class replace {
-    public static void main(String[] args) {
-        String f = "Let me tell you a story of man who did Cyber Scam 2024";
-        String g = f.replaceAll("(?i)cyber scam 2024", "Fake QR code scam");
-        System.out.println(g);
-
-    }
-}
